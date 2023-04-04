74c2e6dbb7127927ede3eae69beeeab8e13aa314
YunBeomOh <dbsqja353@naver.com>
2023-04-04 12:13:05 +0900

    C# Day01

---
+++ Day01/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+// 네임스페이스 ConsoleApp1 
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// '/' 슬래시 기호 3개 입력하면 XML 주석처리 가능
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        ///  메인 엔트리 포인트
+        /// </summary>
+        /// <param name="args">콘솔 매개변수</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow C#");
+        }
+    }
+}
