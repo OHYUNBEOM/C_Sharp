d1e6c7ad1b1613b86d671393fc8f1d6d2e082d12
YunBeomOh <dbsqja353@naver.com>
2023-04-12 16:27:07 +0900

    23_04_12 Day52 MDI

---
+++ Day07/Day07WinApp/wf09_dbhandling/FrmMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wf09_dbhandling
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            // 1.연결 문자열 생성
+            string connectionString = "Data Source=localhost;Initial Catalog=Northwind;Persist Security Info=True;User ID=sa;Password=12345";
+            // 2.DB 연결을 위해 Connection 객체 생성
+            SqlConnection conn = new SqlConnection(connectionString);
+            // 3.DB연결
+            conn.Open();
+
+            // 4.DB처리를 위한 쿼리 작성
+            string selQuery = @"SELECT CustomerID 
+                              ,CompanyName
+                              ,ContactName
+                              ,ContactTitle
+                              ,Address
+                              ,City
+                              ,Region
+                              ,PostalCode
+                              ,Country
+                              ,Phone
+                              ,Fax
+                          FROM Customers";
+            // SqlDataAdapter 생성
+            SqlDataAdapter adapter = new SqlDataAdapter(selQuery,conn);
+            //SqlCommand selCmd = new SqlCommand(selQuery, conn);
+            //selCmd.Connection = conn;
+
+            // 5.리더 객체 생성, 값 넘겨줌
+            //SqlDataReader reader = selCmd.ExecuteReader();
+
+            // 6. 데이터셋으로 전달
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+            // 7. 데이터그리드뷰에 바인딩하기위한 BindingSource 생성
+            BindingSource source = new BindingSource();
+
+            // 8. 데이터그리드뷰의 데이터 소스에 데이터 셋 할당
+            source.DataSource = ds.Tables[0];
+            DgvNorthwind.DataSource = source;
+
+            // 9. DB Close
+            conn.Close();
+        }
+    }
+}
---
+++ Day07/Day07WinApp/wf11_mditest/FrmMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wf11_mditest
+{
+    public partial class FrmMain : Form
+    {
+        FrmChild1 child1 = null;
+        FrmChild2 child2 = null;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void MniForm1_Click(object sender, EventArgs e)
+        {//폼1 클릭 시
+            child1 = new FrmChild1();
+            child1.TopLevel = false;
+            //toplevel을 false로 수정해야 최상위 클래스를 FrmMain으로 잡고 그밑에다 생성
+            this.Controls.Add(child1);
+            child1.Show();
+        }
+
+        private void MniForm2_Click(object sender, EventArgs e)
+        {//폼2 클릭 시
+            child2=new FrmChild2();
+            child2.TopLevel = false;
+            this.Controls.Add(child2);
+            child2.Show();
+        }
+
+        private void MniExit_Click(object sender, EventArgs e)
+        {//끝내기 클릭
+            Application.Exit();
+        }
+
+        private void MniAbout_Click(object sender, EventArgs e)
+        {//이 프로그램 클릭 시
+            FrmAbout about = new FrmAbout();
+            about.ShowDialog();
+        }
+    }
+}
