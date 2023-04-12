af0c5d09443450eee25417942f2a243b0ca48912
YunBeomOh <dbsqja353@naver.com>
2023-04-12 12:27:59 +0900

    23_04_12 Day 52

---
+++ Day07/Day07ConsoleApp/cs26_delegate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs26_delegate
+{
+    #region<대리자선언>
+    //대리자 사용 선언
+    delegate int CalcDelegate(int a, int b);
+    #endregion<대리자선언>
+    delegate int Compare(int a, int b);
+    #region<일반 클래스>
+    class Calc
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        //static으로 선언 시 실행과 동시에 메모리에 올라감
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    #endregion<일반 클래스>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region<일반 클래스>
+            Calc normalCalc = new Calc();
+            int x = 10, y = 15;
+            int res = normalCalc.Plus(x, y);
+            Console.WriteLine(res);
+            res = Calc.Minus(x, y);//Minus 메서드는 static이기에 바로 사용 가능
+            Console.WriteLine(res);
+            #endregion
+
+            #region<대리자>
+            Console.WriteLine("== 대리자 방식 ==");
+            //대리자 사용 방식
+            x = 30; y = 20;
+            Calc delCalc = new Calc();
+            CalcDelegate Callback;
+
+            Callback = new CalcDelegate(delCalc.Plus);
+            int res2 = Callback(x, y);
+            Console.WriteLine(res2);
+
+            Callback = new CalcDelegate(Calc.Minus);
+            res2=Callback(x, y);
+            Console.WriteLine(res2);
+            #endregion
+            
+        }
+    }
+}
---
+++ Day07/Day07ConsoleApp/cs28_event/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs28_event
+{
+    //람다 식
+    class sample
+    {
+        private int valueA;
+        public int ValueA
+        {
+            //get {return valueA;}
+            //set {valueA=value;}
+            get => ValueA;
+            set => ValueA = value;
+        }
+    }
+    // 1.대리자 생성
+    delegate void EventHandler(string message);
+    class CustomNotifier
+    {
+        // 2.이벤트 준비(대리자)
+        public event EventHandler SomethingChanged;
+        public void DoSomething(int number)
+        {
+            int temp = number % 10;
+            if(temp!=0 && temp%3==0)
+            {
+                // 3.이벤트 발생 상황에서 이벤트 수행
+                SomethingChanged(string.Format("{0} : odd", number));
+            }
+        }
+    }
+    internal class Program
+    {
+        // 4.이벤트가 대신 호출할 메서드
+        static void CustomHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            CustomNotifier notifier = new CustomNotifier();
+            notifier.SomethingChanged += new EventHandler(CustomHandler);
+            for(int i=0; i<=30; i++)
+            {
+                notifier.DoSomething(i);
+            }
+        }
+    }
+}
