65fa3bb586221940e91f04d248f805f2f2cc2c6c
YunBeomOh <dbsqja353@naver.com>
2023-04-14 17:19:12 +0900

    23_04_14 Day54 Thread,BookRentalShop

---
+++ Day09/Day09App/BookRentalShop/Helpers/Commons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalShop.Helpers
+{
+    internal class Commons
+    {
+        //static으로 선언하였기에 BookRentalShop 하위 모든 프로그램에서 사용 가능
+        public static readonly string ConnString = "Server=localhost;"+
+                                            "Port=3306;"+
+                                            "Database=bookrentalShop;"+
+                                            "Uid=root;"+
+                                            "Pwd=12345";
+    }
+}
---
+++ Day09/Day09App/wf14_thread/FrmMain.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace wf14_thread
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtNumber = new System.Windows.Forms.TextBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.PgbCalculate = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblResult = new System.Windows.Forms.Label();
+            this.Worker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "입력(수)";
+            // 
+            // TxtNumber
+            // 
+            this.TxtNumber.Location = new System.Drawing.Point(101, 32);
+            this.TxtNumber.Name = "TxtNumber";
+            this.TxtNumber.Size = new System.Drawing.Size(100, 21);
+            this.TxtNumber.TabIndex = 1;
+            this.TxtNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(217, 32);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 2;
+            this.BtnStart.Text = "시작";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(298, 32);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 3;
+            this.BtnCancel.Text = "취소";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // PgbCalculate
+            // 
+            this.PgbCalculate.Location = new System.Drawing.Point(40, 77);
+            this.PgbCalculate.Name = "PgbCalculate";
+            this.PgbCalculate.Size = new System.Drawing.Size(319, 23);
+            this.PgbCalculate.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "결과";
+            // 
+            // LblResult
+            // 
+            this.LblResult.AutoSize = true;
+            this.LblResult.Location = new System.Drawing.Point(99, 125);
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(0, 12);
+            this.LblResult.TabIndex = 6;
+            // 
+            // Worker
+            // 
+            this.Worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Worker_DoWork);
+            this.Worker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.Worker_ProgressChanged);
+            this.Worker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.Worker_RunWorkerCompleted);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 159);
+            this.Controls.Add(this.LblResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PgbCalculate);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.TxtNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "BackgroundWorker 테스트";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtNumber;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.ProgressBar PgbCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblResult;
+        private System.ComponentModel.BackgroundWorker Worker;
+    }
+}
+
